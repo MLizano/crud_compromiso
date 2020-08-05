b78dec3b02b6a9db241406031c207324dafaa240
MLizano <lizanocr@hotmail.com>
2020-08-05 07:59:55 -0600

    conexion de Personas y mejora de la DB

--- Crud_Compromiso/src/main/java/com/crud/comp/modelo/Persona.java
+++ Crud_Compromiso/src/main/java/com/crud/comp/modelo/Persona.java
@@ -8,13 +8,13 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Compromiso_Personas")
+@Table(name = "Compromiso_Personas_pruebas")
 public class Persona {
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	@Column(name="Usuario_Id")
-	private int Usuario_Id;
+	private Integer  Usuario_Id;
 	
 	@Column(name="Usuario_Nombre")
 	private String Usuario_Nombre;
@@ -35,22 +35,22 @@ public class Persona {
 	private String Apellido_Materno;
 	
 	@Column(name="Id_Puesto")
-	private int Id_Puesto;
+	private Integer  Id_Puesto;
 	
 	@Column(name="Id_Ceco")
-	private int Id_Ceco;
+	private Integer  Id_Ceco;
 	
 	@Column(name="Tipo_Usr")
 	private String Tipo_Usr;
 	
-	@Column(name="Puesto_Clave", nullable=true)
+	@Column(name="Puesto_Clave", nullable= true)
 	private String Puesto_Clave;
 	
 	@Column(name="Id_Organizacion")
-	private int Id_Organizacion;
+	private Integer  Id_Organizacion;
 	
-	@Column(name="Id_Jefe")
-	private int Id_Jefe;
+	@Column(name="Id_Jefe", nullable= true)
+	private Integer  Id_Jefe;
 	
 	
 	
@@ -60,9 +60,9 @@ public class Persona {
 
 
 
-	public Persona(int usuario_Id, String usuario_Nombre, String usuario_Login, String usuario_Email, String nombre,
-			String apellido_Paterno, String apellido_Materno, int id_Puesto, int id_Ceco, String tipo_Usr,
-			String puesto_Clave, int id_Organizacion, int id_Jefe) {
+	public Persona(Integer usuario_Id, String usuario_Nombre, String usuario_Login, String usuario_Email, String nombre,
+			String apellido_Paterno, String apellido_Materno, Integer id_Puesto, Integer id_Ceco, String tipo_Usr,
+			String puesto_Clave, Integer id_Organizacion, Integer id_Jefe) {
 		super();
 		Usuario_Id = usuario_Id;
 		Usuario_Nombre = usuario_Nombre;
@@ -81,85 +81,109 @@ public class Persona {
 
 
 
-	public int getUsuario_Id() {
+	public Integer getUsuario_Id() {
 		return Usuario_Id;
 	}
-	public void setUsuario_Id(int usuario_Id) {
+
+	public void setUsuario_Id(Integer usuario_Id) {
 		Usuario_Id = usuario_Id;
 	}
+
 	public String getUsuario_Nombre() {
 		return Usuario_Nombre;
 	}
+
 	public void setUsuario_Nombre(String usuario_Nombre) {
 		Usuario_Nombre = usuario_Nombre;
 	}
+
 	public String getUsuario_Login() {
 		return Usuario_Login;
 	}
+
 	public void setUsuario_Login(String usuario_Login) {
 		Usuario_Login = usuario_Login;
 	}
+
 	public String getUsuario_Email() {
 		return Usuario_Email;
 	}
+
 	public void setUsuario_Email(String usuario_Email) {
 		Usuario_Email = usuario_Email;
 	}
+
 	public String getNombre() {
 		return Nombre;
 	}
+
 	public void setNombre(String nombre) {
 		Nombre = nombre;
 	}
+
 	public String getApellido_Paterno() {
 		return Apellido_Paterno;
 	}
+
 	public void setApellido_Paterno(String apellido_Paterno) {
 		Apellido_Paterno = apellido_Paterno;
 	}
+
 	public String getApellido_Materno() {
 		return Apellido_Materno;
 	}
+
 	public void setApellido_Materno(String apellido_Materno) {
 		Apellido_Materno = apellido_Materno;
 	}
-	public int getId_Puesto() {
+
+	public Integer getId_Puesto() {
 		return Id_Puesto;
 	}
-	public void setId_Puesto(int id_Puesto) {
+
+	public void setId_Puesto(Integer id_Puesto) {
 		Id_Puesto = id_Puesto;
 	}
-	public int getId_Ceco() {
+
+	public Integer getId_Ceco() {
 		return Id_Ceco;
 	}
-	public void setId_Ceco(int id_Ceco) {
+
+	public void setId_Ceco(Integer id_Ceco) {
 		Id_Ceco = id_Ceco;
 	}
+
 	public String getTipo_Usr() {
 		return Tipo_Usr;
 	}
+
 	public void setTipo_Usr(String tipo_Usr) {
 		Tipo_Usr = tipo_Usr;
 	}
+
 	public String getPuesto_Clave() {
 		return Puesto_Clave;
 	}
+
 	public void setPuesto_Clave(String puesto_Clave) {
 		Puesto_Clave = puesto_Clave;
 	}
-	public int getId_Organizacion() {
+
+	public Integer getId_Organizacion() {
 		return Id_Organizacion;
 	}
-	public void setId_Organizacion(int id_Organizacion) {
+
+	public void setId_Organizacion(Integer id_Organizacion) {
 		Id_Organizacion = id_Organizacion;
 	}
-	public int getId_Jefe() {
+
+	public Integer getId_Jefe() {
 		return Id_Jefe;
 	}
-	public void setId_Jefe(int id_Jefe) {
+
+	public void setId_Jefe(Integer id_Jefe) {
 		Id_Jefe = id_Jefe;
 	}
 
-	
-	
+
 }
--- Crud_Compromiso/src/main/java/com/crud/comp/modelo/Rubro.java
+++ Crud_Compromiso/src/main/java/com/crud/comp/modelo/Rubro.java
@@ -2,8 +2,8 @@ package com.crud.comp.modelo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -13,15 +13,17 @@ public class Rubro {
 
 	// campos de caracter informativo --------------------------
 	@Id
-	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@GeneratedValue (strategy = GenerationType.IDENTITY)
 	@Column(name = "Rubro_id")
-	private int Rubro_id;
+	private Integer Rubro_id;
 
+	/*
 	@Column(name = "Emp_id")
-	private int Emp_id;
+	private Integer Emp_id;
+	*/
 
 	@Column(name = "Estado_id")
-	private int Estado_id;
+	private Integer Estado_id;
 
 	@Column(name = "Rubro_Nombre")
 	private String Rubro_Nombre;
@@ -30,11 +32,12 @@ public class Rubro {
 	private String Rubro_Fec_Actualizacion;
 
 	@Column(name = "Tipo_Ejecucion_id")
-	private int Tipo_Ejecucion_id;
+	private Integer Tipo_Ejecucion_id;
 
 	@Column(name = "Ind_Solicitud")
-	private int Ind_Solicitud;
+	private Integer Ind_Solicitud;
 
+	
 	// campos modificables -----------------------------------
 	@Column(name = "Id_Validador_Categoria")
 	private Integer Id_Validador_Categoria;
@@ -51,16 +54,19 @@ public class Rubro {
 	@Column(name = "Id_Organizacion")
 	private String Id_Organizacion;
 
+	
+	
 	public Rubro() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Rubro(int rubro_id, int emp_id, int estado_id, String rubro_Nombre, String rubro_Fec_Actualizacion,
-			int tipo_Ejecucion_id, int ind_Solicitud, Integer id_Validador_Categoria, String en_Stock, String estatus,
-			String tipo_Rubro, String id_Organizacion) {
+	
+	public Rubro(Integer rubro_id, Integer estado_id, String rubro_Nombre,
+			String rubro_Fec_Actualizacion, Integer tipo_Ejecucion_id, Integer ind_Solicitud,
+			Integer id_Validador_Categoria, String en_Stock, String estatus, String tipo_Rubro,
+			String id_Organizacion) {
 		super();
 		Rubro_id = rubro_id;
-		Emp_id = emp_id;
 		Estado_id = estado_id;
 		Rubro_Nombre = rubro_Nombre;
 		Rubro_Fec_Actualizacion = rubro_Fec_Actualizacion;
@@ -73,43 +79,30 @@ public class Rubro {
 		Id_Organizacion = id_Organizacion;
 	}
 
-	public String getRubro_Fec_Actualizacion() {
-		return Rubro_Fec_Actualizacion;
-	}
-
-	public void setRubro_Fec_Actualizacion(String rubro_Fec_Actualizacion) {
-		Rubro_Fec_Actualizacion = rubro_Fec_Actualizacion;
-	}
-
-	public Integer getId_Validador_Categoria() {
-		return Id_Validador_Categoria;
-	}
-
-	public void setId_Validador_Categoria(Integer id_Validador_Categoria) {
-		Id_Validador_Categoria = id_Validador_Categoria;
-	}
-
-	public int getRubro_id() {
+	
+	public Integer getRubro_id() {
 		return Rubro_id;
 	}
 
-	public void setRubro_id(int rubro_id) {
+	public void setRubro_id(Integer rubro_id) {
 		Rubro_id = rubro_id;
 	}
 
-	public int getEmp_id() {
+	/*
+	public Integer getEmp_id() {
 		return Emp_id;
 	}
 
-	public void setEmp_id(int emp_id) {
+	public void setEmp_id(Integer emp_id) {
 		Emp_id = emp_id;
 	}
+	*/
 
-	public int getEstado_id() {
+	public Integer getEstado_id() {
 		return Estado_id;
 	}
 
-	public void setEstado_id(int estado_id) {
+	public void setEstado_id(Integer estado_id) {
 		Estado_id = estado_id;
 	}
 
@@ -121,20 +114,36 @@ public class Rubro {
 		Rubro_Nombre = rubro_Nombre;
 	}
 
-	public int getTipo_Ejecucion_id() {
+	public String getRubro_Fec_Actualizacion() {
+		return Rubro_Fec_Actualizacion;
+	}
+
+	public void setRubro_Fec_Actualizacion(String rubro_Fec_Actualizacion) {
+		Rubro_Fec_Actualizacion = rubro_Fec_Actualizacion;
+	}
+
+	public Integer getTipo_Ejecucion_id() {
 		return Tipo_Ejecucion_id;
 	}
 
-	public void setTipo_Ejecucion_id(int tipo_Ejecucion_id) {
+	public void setTipo_Ejecucion_id(Integer tipo_Ejecucion_id) {
 		Tipo_Ejecucion_id = tipo_Ejecucion_id;
 	}
 
-	public int getInd_Solicitud() {
+	public Integer getInd_Solicitud() {
 		return Ind_Solicitud;
 	}
 
-	public void setInd_Solicitud(int ind_Solicitud) {
+	public void setInd_Solicitud(Integer ind_Solicitud) {
 		Ind_Solicitud = ind_Solicitud;
+	}
+
+	public Integer getId_Validador_Categoria() {
+		return Id_Validador_Categoria;
+	}
+
+	public void setId_Validador_Categoria(Integer id_Validador_Categoria) {
+		Id_Validador_Categoria = id_Validador_Categoria;
 	}
 
 	public String getEn_Stock() {
@@ -169,4 +178,7 @@ public class Rubro {
 		Id_Organizacion = id_Organizacion;
 	}
 
+	
+	
+	
 }
